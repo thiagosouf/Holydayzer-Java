2d11661633bac8a0dc0d4e7da88dbf3da8fb2593
Thiago Ferreira <62024096+thiagosouf@users.noreply.github.com>
2023-01-13 14:45:26 -0300

    final

---
+++ Feriados.java
@@ -0,0 +1,20 @@
+
+public class Feriados {
+    String[] dataFeriados = new String[] {
+            "01/01/2023",
+            "21/02/2023",
+            "17/04/2023",
+            "21/04/2023",
+            "01/05/2023",
+            "08/06/2023",
+            "07/09/2023",
+            "12/10/2023",
+            "02/11/2023",
+            "15/11/2023",
+            "25/12/2023" };
+    String[] nomeFeriados = new String[] {
+            "Confraternização mundial", "Carnaval", "Páscoa", "Tiradentes", "Dia do trabalho", "Corpus Christi",
+            "Independência do Brasil", "Nossa Senhora Aparecida", "Finados", "Proclamação da República", "Natal" };
+    // System.out.println("Verificar Feriado");
+
+}
